1e2fd89004ae1e49156c4ba174a4ec08caf89217
NeiilSuri <81522963+NeiilSuri@users.noreply.github.com>
2021-10-01 22:46:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let a;
+let date;
+let time;
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+var noon = options + 
+function send(){
+    if(options < ""){
+
+    }
+}
